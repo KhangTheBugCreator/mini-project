a83dd3915e7ae2b50ffc7355484bd9895b290853
KhangTheBugCreator <110124083@st.tvu.edu.vn>
2026-05-22 09:27:04 +0700

    shopping-cart

--- lib.js
+++ lib.js
@@ -1,34 +1,113 @@
-const colorCode = document.getElementById("colorCode");
-const generateBtn = document.getElementById("generateBtn");
-const historyList = document.getElementById("historyList");
-const clearBtn = document.getElementById("clearBtn");
+const products = [
+  {
+    id: 1,
+    name: "Rose Flower",
+    price: 30000,
+  },
 
-generateBtn.onclick = function () {
-  const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  {
+    id: 2,
+    name: "Tulip Flower",
+    price: 25000,
+  },
 
-  document.querySelector("body").style.background = randomColor;
+  {
+    id: 3,
+    name: "Sunflower",
+    price: 20000,
+  },
+];
 
-  colorCode.innerHTML = randomColor;
-  colorCode.style.color = randomColor;
+const productList = document.getElementById("productList");
+const cartList = document.getElementById("cartList");
+const totalPrice = document.getElementById("totalPrice");
 
-  const li = document.createElement("li");
-  const textColor = document.createTextNode(randomColor);
-  li.appendChild(textColor);
-  li.style.color = randomColor;
+//render product
 
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerHTML = "X";
-  deleteBtn.setAttribute("class", "deleteBtn");
-  li.appendChild(deleteBtn);
+function renderProduct() {
+  productList.innerHTML = "";
+  for (const product of products) {
+    productList.innerHTML += `
+      <div class="product">
+        <h3>${product.name}</h3>
+        <p>Price: ${product.price} VND</p>
+        <button onclick="addToCart(${product.id})">Add To Cart</button>
+      </div>
+    `;
+  }
+}
 
-  historyList.appendChild(li);
-  deleteBtn.onclick = function () {
-    historyList.removeChild(li);
-  };
-};
-clearBtn.onclick = function () {
-  historyList.innerHTML = "";
-  document.querySelector("body").style.background = "#ffffff";
-  colorCode.innerHTML = "#FFFFFF";
-  colorCode.style.color = "black";
-};
+// add Item to  Cart
+let cart = [];
+function addToCart(id) {
+  const product = products.find((item) => item.id === id); //find array
+  const cartItem = cart.find((item) => item.id === id);
+
+  if (cartItem) {
+    cartItem.quantity++;
+  } else {
+    cart.push({
+      id: product.id,
+      name: product.name,
+      price: product.price,
+      quantity: 1,
+    });
+  }
+  renderCart();
+}
+//render Cart
+
+function renderCart() {
+  cartList.innerHTML = "";
+  let total = 0;
+  for (const item of cart) {
+    total += item.price * item.quantity;
+    cartList.innerHTML += `
+      <div class="cart-item">
+        <h3>
+          ${item.name}
+        </h3>
+        <p>Price: ${item.price} VND</p>
+        <p>Quantity: ${item.quantity}</p>
+        <button onclick="decreaseQuantity(${item.id})">-</button>
+        <button onclick="increaseQuantity(${item.id})">+</button>
+        <button onclick="removeItem(${item.id})">Delete</button>
+      </div>
+    
+    
+    `;
+  }
+  totalPrice.innerHTML = total;
+}
+
+// Increase quantity
+function increaseQuantity(id) {
+  for (const item of cart) {
+    if (item.id === id) {
+      item.quantity++;
+    }
+  }
+
+  renderCart();
+}
+// Decrease quantity
+function decreaseQuantity(id) {
+  for (const item of cart) {
+    if (item.id === id) {
+      item.quantity--;
+
+      if (item.quantity <= 0) {
+        removeItem(id);
+        return;
+      }
+    }
+  }
+
+  renderCart();
+}
+// Remove item
+function removeItem(id) {
+  cart = cart.filter((item) => item.id !== id);
+
+  renderCart();
+}
